ff8c7b17f0d846e89dd7adbc5d9ad6c15ffe23b8
Thembelani Ngema <thembelaningema20@gmail.com>
2025-10-04 10:58:19 +0200

    Final commit with tests

---
+++ src/main/java/thembelani/java/tutorials/Song.java
@@ -0,0 +1,23 @@
+package thembelani.java.tutorials;
+
+public class Song {
+
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ":" + this.duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+
+}
